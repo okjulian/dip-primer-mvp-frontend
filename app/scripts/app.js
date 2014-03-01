72d4fdc8d69eabac32b93ca3cb3e7a3f68403975
El Juli <mayorga.julian@gmail.com>
2014-03-01 12:13:21 -0300

    feat(inicial): crear proyecto angular

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('lauchApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
